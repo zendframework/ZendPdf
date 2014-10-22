d08f3dd4834fb505f37955b1b0d4a89f57f04ac2
delboy1978uk <delboy1978uk@gmail.com>
2014-10-22 19:56:59 +0100

    Deprecated class now extends new class

--- library/ZendPdf/Util/RecursivelyIteratableObjectsContainer.php
+++ library/ZendPdf/Util/RecursivelyIteratableObjectsContainer.php
@@ -17,34 +17,6 @@ namespace ZendPdf\Util;
  * @subpackage Zend_PDF_Util
  * @deprecated Use RecursivelyIterableObjectsContainer instead
  */
-class RecursivelyIteratableObjectsContainer implements \RecursiveIterator, \Countable
+class RecursivelyIteratableObjectsContainer extends RecursivelyIterableObjectsContainer implements \RecursiveIterator, \Countable
 {
-    protected $_objects = array();
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function __construct(array $objects) { $this->_objects = $objects; }
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function current()      { return current($this->_objects);            }
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function key()          { return key($this->_objects);                }
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function next()         { return next($this->_objects);               }
-    
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function rewind()       { return reset($this->_objects);              }
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function valid()        { return current($this->_objects) !== false;  }
-    
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function getChildren()  { return current($this->_objects);            }
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function hasChildren()  { return count($this->_objects) > 0;          }
-
-    /** @deprecated use RecursivelyIterableObjectsContainer instead */
-    public function count() { return count($this->_objects); }
 }
